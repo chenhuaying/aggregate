c89aa409f906e86bc5a2877e29046703fccc9283
huaying <chenhuaying@sina.cn>
2015-11-25 19:38:49 +0800

    add zmap using container skiplist

---
+++ zmap.go
@@ -0,0 +1,74 @@
+package aggregate
+
+import (
+	"github.com/user/aggregate/iterator"
+	"github.com/user/container"
+	"github.com/user/container/skiplist"
+)
+
+type Zmap struct {
+	*skiplist.SkipList
+}
+
+type ZmapIterator struct {
+	currNode  *skiplist.SkipListNode
+	aggregate *Zmap
+}
+
+func NewZmap() *Zmap {
+	return &Zmap{skiplist.NewSkipList()}
+}
+
+func (m *Zmap) Iterator() iterator.Iterator {
+	return &ZmapIterator{currNode: m.First(), aggregate: m}
+}
+
+func (s *Zmap) Find(key container.Comparer) iterator.Iterator {
+	node := s.SearchNode(key)
+	return &ZmapIterator{currNode: node, aggregate: s}
+}
+
+func (s *Zmap) LowerBound(key container.Comparer) iterator.Iterator {
+	node := s.LowerBoundNode(key)
+	return &ZmapIterator{currNode: node, aggregate: s}
+}
+
+func (i *ZmapIterator) Next() {
+	i.currNode = i.currNode.Next()
+}
+
+func (i *ZmapIterator) Prev() {
+	i.currNode = i.currNode.Prev()
+}
+
+func (i *ZmapIterator) First() interface{} {
+	if i.currNode != nil {
+		return i.currNode.Key()
+	} else {
+		return nil
+	}
+}
+
+func (i *ZmapIterator) Second() interface{} {
+	if i.currNode != nil {
+		return i.currNode.Value()
+	} else {
+		return nil
+	}
+}
+
+func (i *ZmapIterator) Begin() bool {
+	if i.currNode == i.aggregate.First() {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (i *ZmapIterator) End() bool {
+	if i.currNode == nil {
+		return true
+	} else {
+		return false
+	}
+}
---
+++ zmap_test.go
@@ -0,0 +1,99 @@
+package aggregate
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/user/container"
+)
+
+type KeyUint uint32
+
+func (k KeyUint) Less(x container.Comparer) bool {
+	return k < x.(KeyUint)
+}
+
+func TestZmapNew(t *testing.T) {
+	z := NewZmap()
+	if z.IsEmpty() != true {
+		t.Error("new must be empty")
+	}
+
+	if z.First() != nil {
+		t.Error("new Zmap first must be nil")
+	}
+}
+
+func TestZmapIteratorBeginAndEnd(t *testing.T) {
+	z := NewZmap()
+	z.Insert(KeyUint(3), "test-3")
+	for j := 0; j < 10; j++ {
+		key := 10 + j
+		value := fmt.Sprintf("test-%d", key)
+		z.Insert(KeyUint(key), value)
+	}
+	z.Insert(KeyUint(25), "test-25")
+	z.Insert(KeyUint(35), "test-35")
+
+	i := z.Find(KeyUint(3))
+	fmt.Printf("is Begin %v, %d %v\n", i.Begin(), i.First(), i.Second())
+	if !i.Begin() {
+		t.Error("3 is the begin, why this case failed?")
+	}
+
+	i = z.Find(KeyUint(21))
+	if !i.End() {
+		t.Error("21 is not in the Zmap, why iterator not at end?")
+	}
+}
+
+func TestZmapIterator(t *testing.T) {
+	z := NewZmap()
+	z.Insert(KeyUint(3), "test-3")
+	for j := 0; j < 10; j++ {
+		key := 10 + j
+		value := fmt.Sprintf("test-%d", key)
+		z.Insert(KeyUint(key), value)
+	}
+	z.Insert(KeyUint(25), "test-25")
+	z.Insert(KeyUint(35), "test-35")
+
+	//for i := z.Iterator(); !i.End(); i.Next() {
+	//	fmt.Println(i.First(), i.Second())
+	//}
+
+	i := z.Find(KeyUint(13))
+	// must use type assert, otherwise it will be failed
+	if i.First().(KeyUint) != 13 || i.Second().(string) != "test-13" {
+		t.Error("Zmap find error node!", i.First(), i.Second())
+	}
+
+	dest := []uint32{13, 14, 15, 16, 17, 18, 19, 25, 35}
+	for j, c := i, 0; !j.End(); j.Next() {
+		if uint32(i.First().(KeyUint)) != dest[c] {
+			t.Error("next iterator error")
+		}
+		c += 1
+	}
+
+	dest = []uint32{13, 12, 11, 10, 3}
+	for j, c := z.Find(KeyUint(13)), 0; !j.End(); j.Prev() {
+		if uint32(j.First().(KeyUint)) != dest[c] {
+			t.Error("next iterator error")
+		}
+		c += 1
+	}
+
+	dest = []uint32{25, 35}
+	for j, c := z.LowerBound(KeyUint(20)), 0; !j.End(); j.Next() {
+		if uint32(j.First().(KeyUint)) != dest[c] {
+			t.Error("prev iterator error")
+		}
+		c++
+	}
+
+	i = z.LowerBound(KeyUint(40))
+	if !i.End() {
+		t.Error("find 40 node?")
+	}
+}
